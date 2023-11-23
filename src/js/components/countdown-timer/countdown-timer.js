ce62595a999325591dbb12c7970c8d6cba69dfac
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2023-11-23 01:52:02 +0100

    Implement callbacks, a getter and methods to the countdown-timer component

--- src/js/components/countdown-timer/countdown-timer.js
+++ src/js/components/countdown-timer/countdown-timer.js
@@ -20,7 +20,6 @@ template.innerHTML = `
         font-size: 1.5rem;
     }
 </style>
-
 <div id="container">
     <p>Time left: <b id="timer"></b></p>
 </div>
@@ -37,6 +36,11 @@ customElements.define('countdown-timer',
     #timerBoard
 
     /**
+     * Represents the timer.
+     */
+    #timer
+
+    /**
      * Creates an instance of the current type.
      */
     constructor () {
@@ -47,5 +51,68 @@ customElements.define('countdown-timer',
         .appendChild(template.content.cloneNode(true))
 
       this.#timerBoard = this.shadowRoot.querySelector('#container')
+      this.#timer = this.shadowRoot.querySelector('#timer')
+    }
+
+    /**
+     * Attribute to monitor for changes.
+     *
+     * @returns {string[]} A string arrat of attributes to monitor.
+     */
+    static get observedAttributes () {
+      return ['time']
+    }
+
+    /**
+     * Called after the element is inserted into the DOM.
+     */
+    connectedCallback () {
+      if (!this.hasAttribute('time')) {
+        this.setAttribute('time', '20')
+      }
+
+      this.#upgradeProperty('time')
+    }
+
+    /**
+     * Called after the element has been removed from the DOM.
+     */
+    // disconnectedCallbak () {}
+
+    /**
+     * Run the specified instance property
+     * through the class setter.
+     *
+     * @param {string} prop - The property's name.
+     */
+    #upgradeProperty (prop) {
+      if (Object.hasOwnProperty.call(this, prop)) {
+        const value = this[prop]
+        delete this[prop]
+        this[prop] = value
+      }
+    }
+
+    /**
+     * Get the time left on the timer.
+     *
+     * @returns {string} - A string representing the number of seconds left.
+     */
+    get timeLeft () {
+      return this.#timer.textContent
+    }
+
+    /**
+     * Method that start and runs the timer.
+     */
+    startTimer () {}
+
+    /**
+     * Update the rendering for the timer.
+     *
+     * @param {string} time - A string representing the time left to be shown to the user.
+     */
+    updateRender (time) {
+      this.#timer.textContent = `${time}`
     }
   })
